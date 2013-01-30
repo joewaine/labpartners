a260d746a02d3757b6e794eb28875780fbaf7a77
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2013-01-30 17:23:39 -0500

    initialized subway

--- labpartners.rb
+++ labpartners.rb
@@ -1,6 +1,6 @@
 puts "What is the group size?"
-size_of_groups = gets.chomp.to_i
-
+#size_of_groups = gets.chomp.to_i
+size_of_groups = 3
 a = ("a".."z").to_a
 
 a.shuffle
@@ -8,7 +8,7 @@ a.shuffle
 
 number_of_groups = (a.length.to_i/size_of_groups).to_i #=> 8
 
-initial = 0
+initial = number_of_groups -1
 
 number_of_groups.times do
 
@@ -19,7 +19,7 @@ number_of_groups.times do
             array.push(i)
         end
       end
-  initial = initial + 1
+  initial = initial - 1
   print array
 end
 
