74913c85ba57be81e9adee333c1cea1fabd61f19
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2013-01-30 16:35:47 -0500

    maybed worked

--- labpartners.rb
+++ labpartners.rb
@@ -9,15 +9,19 @@ size_of_groups = 3
 number_of_loops = (25/size_of_groups).to_i #=> 8
 
 initial = 0
-
+array_i = 0
 number_of_loops.times do
 
+        array = Array.new
 
-  a.each_with_index.map do |i, idx|
-    "{i}" if idx % number_of_loops == initial
-  end
-
+      a.each_with_index.collect do |i, idx|
+        if idx % number_of_loops == initial
+            array.push(i)
+        end
+      end
+  #3 names to put into an array
   initial = initial + 1
+  print array
 end
   #take the array of names, start with the number 1, each time its divisible
   #by zero, select those names
